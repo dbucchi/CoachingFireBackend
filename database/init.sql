946cf1fcbb0398acf86363c651de884d19809918
Daniele Bucchi <d.bucchi@domotz.com>
2024-02-23 17:34:11 +0100

    add games

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,11 @@
 -- init.sql
 
-CREATE TYPE role AS ENUM ('coach', 'normal_user' );
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'role') THEN
+        CREATE TYPE role AS ENUM ('coach', 'normal_user');
+    END IF;
+END $$;
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
@@ -12,11 +17,17 @@ CREATE TABLE IF NOT EXISTS users (
     creation_time timestamp
 );
 
+CREATE TABLE IF NOT EXISTS games (
+    id SERIAL PRIMARY KEY,
+    name    VARCHAR(255)
+);
+
 CREATE TABLE IF NOT EXISTS appointments (
     id SERIAL PRIMARY KEY,
     coach_id INT REFERENCES users(id),
     appointmet_date timestamp,
     appointment_user_id INT REFERENCES users(id),
+    game_id INT REFERENCES games(id),
     creation_time timestamp
 );
 -- Aggiungi altre query per creare ulteriori tabelle se necessario
